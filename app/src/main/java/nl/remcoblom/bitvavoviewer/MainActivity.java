3902d4de8227ae4adea183792331437a19ef8806
Remco Blom <remcoblom92@hotmail.com>
2020-04-23 21:46:00 +0200

    added extra textviews to show also values of assets seperately

--- app/src/main/java/nl/remcoblom/bitvavoviewer/MainActivity.java
+++ app/src/main/java/nl/remcoblom/bitvavoviewer/MainActivity.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import org.json.JSONException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,7 +19,21 @@ import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView helloWorldTextView;
+    private TextView totalTextView;
+    private TextView assetTextView1;
+    private TextView assetTextView2;
+    private TextView assetTextView3;
+    private TextView assetTextView4;
+    private TextView assetTextView5;
+    private TextView assetTextView6;
+    private TextView assetTextView7;
+    private TextView assetTextView8;
+    private TextView assetTextView9;
+    private TextView assetTextView10;
+    private TextView assetTextView11;
+    private TextView assetTextView12;
+    private TextView assetTextView13;
+    private TextView assetTextView14;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +46,27 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calculate();
+                setTextViews();
             }
         });
 
-        helloWorldTextView = findViewById(R.id.helloWorldTextView);
-        calculate();
+        totalTextView = findViewById(R.id.totalTextView);
+        assetTextView1 = findViewById(R.id.assetTextView1);
+        assetTextView2 = findViewById(R.id.assetTextView2);
+        assetTextView3 = findViewById(R.id.assetTextView3);
+        assetTextView4 = findViewById(R.id.assetTextView4);
+        assetTextView5 = findViewById(R.id.assetTextView5);
+        assetTextView6 = findViewById(R.id.assetTextView6);
+        assetTextView7 = findViewById(R.id.assetTextView7);
+        assetTextView8 = findViewById(R.id.assetTextView8);
+        assetTextView9 = findViewById(R.id.assetTextView9);
+        assetTextView10 = findViewById(R.id.assetTextView10);
+        assetTextView11 = findViewById(R.id.assetTextView11);
+        assetTextView12 = findViewById(R.id.assetTextView12);
+        assetTextView13 = findViewById(R.id.assetTextView13);
+        assetTextView14 = findViewById(R.id.assetTextView14);
+
+        setTextViews();
     }
 
     @Override
@@ -64,22 +91,33 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void calculate() {
+    private void setTextViews() {
         try {
             InputStream inputStream = getBaseContext().getAssets().open("Bitvavo.properties");
             Properties properties = new Properties();
             properties.load(inputStream);
-            Calculate calculate = new Calculate(properties);
+            AssetsInEURTask assetsInEURTask = new AssetsInEURTask(properties);
             ExecutorService service = Executors.newSingleThreadExecutor();
-            double result = service.submit(calculate).get();
-            System.out.println("Total: " + result);
-            helloWorldTextView.setText(String.valueOf(result));
-        } catch (IOException e) {
+
+            Map<Currency,Double> assetsInEUR = service.submit(assetsInEURTask).get();
+            assetTextView1.setText(Currency.EUR + ": " + assetsInEUR.get(Currency.EUR));
+            assetTextView2.setText(Currency.BTC + ": " + assetsInEUR.get(Currency.BTC));
+            assetTextView3.setText(Currency.LTC + ": " + assetsInEUR.get(Currency.LTC));
+            assetTextView4.setText(Currency.XRP + ": " + assetsInEUR.get(Currency.XRP));
+            assetTextView5.setText(Currency.XLM + ": " + assetsInEUR.get(Currency.XLM));
+            assetTextView6.setText(Currency.VET + ": " + assetsInEUR.get(Currency.VET));
+            assetTextView7.setText(Currency.VTHO + ": " + assetsInEUR.get(Currency.VTHO));
+            assetTextView8.setText(Currency.NEO + ": " + assetsInEUR.get(Currency.NEO));
+            assetTextView9.setText(Currency.GAS + ": " + assetsInEUR.get(Currency.GAS));
+            assetTextView10.setText(Currency.ONT + ": " + assetsInEUR.get(Currency.ONT));
+            assetTextView11.setText(Currency.ONG + ": " + assetsInEUR.get(Currency.ONG));
+            assetTextView12.setText(Currency.XVG + ": " + assetsInEUR.get(Currency.XVG));
+            assetTextView13.setText(Currency.TRX + ": " + assetsInEUR.get(Currency.TRX));
+            assetTextView14.setText(Currency.WTC + ": " + assetsInEUR.get(Currency.WTC));
+            totalTextView.setText("Total: " + assetsInEUR.values().stream().mapToDouble(i -> i).sum());
+
+        } catch (IOException | InterruptedException | ExecutionException e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
             e.printStackTrace();
         }
     }
